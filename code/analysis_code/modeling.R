f141c67f6bc90da2e16ae3d251fa258b14cabc9b
Ehsan Suez <Ehsan.suez@uga.edu>
2021-10-22 15:53:45 -0400

    Ehsan's edit

--- code/analysis_code/modeling.R
+++ code/analysis_code/modeling.R
@@ -69,4 +69,120 @@ aug_sp %>% roc_auc(truth = CoughYN, .pred_Yes)
 #table_file = here("results", "resulttable.rds")
 #saveRDS(lmtable, file = table_file)
 
+
+
+#---------------------------
+#---------------------------
+#Ehsan starts here:
+
+
+#Create recipe
+Recipe_BT <- recipe(BodyTemp ~ ., data = train_data)
+
+#Define model 
+linear_mod <- linear_reg() %>% 
+  set_engine("lm") %>%
+  set_mode("regression")
+
+#Create workflow that adds recipe and model
+BT_lm_wflow <- 
+  workflow() %>% 
+  add_model(linear_mod) %>% 
+  add_recipe(Recipe_BT)
+
+# Using the Workflow, model fitting to Recipe_BT
+BT_fit <- 
+  BT_lm_wflow %>% 
+  fit(data = train_data)
+
+#See
+BT_fit %>% 
+  extract_fit_parsnip() %>% 
+  tidy()
+
+### Using Workflows to Make Predictions
+
+#predict values in test data
+predict(BT_fit, test_data)
+
+#predictions with augment
+BT_aug_test <- 
+  augment(BT_fit, test_data)
+
+### Using RMSE to Assess Model Fit
+
+#Calculate RMSE for test data
+BT_aug_test %>%
+  rmse(truth = BodyTemp, .pred)
+
+#Predict outcomes using augment in training data
+BT_aug_train <- 
+  augment(BT_fit, train_data)
+
+#Calculate RSME for training data
+BT_aug_train %>%
+  rmse(truth = BodyTemp, .pred)
+
+
+
+#Creates recipe
+Recipe_BT2 <- recipe(BodyTemp ~ RunnyNose, data = train_data)
+
+#Define model 
+linear_mod <- linear_reg() %>% 
+  set_engine("lm") %>%
+  set_mode("regression")
+
+#Create workflow that adds our recipe and model
+BT2_wflow <- 
+  workflow() %>% 
+  add_model(linear_mod) %>% 
+  add_recipe(Recipe_BT2)
+
+#model fitting to workflow
+BT2_fit <- 
+  BT2_wflow %>% 
+  fit(data = train_data)
+
+#see
+BT2_fit %>% 
+  extract_fit_parsnip() %>% 
+  tidy()
+
+### Using Workflows to Make Predictions
+
+#predict values in test data
+predict(BT2_fit, test_data)
+
+
+#Use the augment() function to predict outcomes in the test data.
+
+#Predict outcomes using augment in test data
+BT2_aug_test <- 
+  augment(BT2_fit, test_data)
+
+### Using RMSE to Assess Model Fit
+
+
+#### Assessing fit for test data predictions
+
+#Make RMSE for test data predictions.
+BT2_aug_test %>%
+  rmse(truth = BodyTemp, .pred)
+
+#RMSE = 1.19
+
+####RMSE for Body Temperature as a function of Runny Nose in Train Data.
+
+#Predict outcomes using augment in training data.
+BT2_aug_train <- 
+  augment(BT2_fit, train_data)
+
+#Calculate RMSE for training data.
+BT2_aug_train %>%
+  rmse(truth = BodyTemp, .pred)
+
+#RMSE = 1.19
+
+
   
